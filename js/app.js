c31b2343307614f22070e277b02e4271223e1413
Christopher L Chapman <ChristopherLChapman42@gmail.com>
2018-05-09 02:13:36 -0700

    sales and staff data loading, missing hourly totals

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 'use strict';
 
+// ==================================
+// Some global variables, mostly set by paramters from company owner
+// ==================================
+
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'] // length is 15
-var nameStore = ['First and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
-var storeList = [];
+var storeList = []; // this is populated when we add Store object 
+// var nameStore = ['First and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+
+var minStaff = 2; // every store will always have 2 cookie workers
+var staffWorkload = 20; // how many cookies/hr can each staff member handle
+
+var salesHeader = ['Store Name'].concat(hours, 'Daily Location Total');
+var staffHeader = hours.slice();
+staffHeader.unshift('Store Name');
+// I tried using unshift & push on newArray, after newArray = oldArray
+// However, these would also change oldArray.
 
 // ==================================
 // ======= Object Constructor =======
@@ -14,17 +27,20 @@ function Store(storeName, minCustomer, maxCustomer, avgCookie) {
     this.storeName = storeName;
     this.minCustomer = minCustomer;
     this.maxCustomer = maxCustomer;
-    this.avgCookie = avgCookie; 
-    this.hrCustomer = []; 
-    this.hrCookies = []; 
-    this.totalCookies = 0;    
-    storeList.push(this); 
-    this.fakeCustomerVisits(); 
+    this.avgCookie = avgCookie;
+    this.hrCustomer = [];
+    this.hrCookies = [];
+    this.totalCookies = 0;
+    storeList.push(this);
+    this.fakeCustomerVisits();
 }
 
-// ==================
-// make some methods: fakeCustomerVisit, render (as table format)
-// ==================
+// ===========================
+// ==== make some methods ====
+// fakeCustomerVisit is guess based on company owner's expectations of hrly customers & avg sales
+// renderSales is output of sales 
+// renderStaff is report of staff needed based on hourly cookie sales
+// ===========================
 
 Store.prototype.fakeCustomerVisits = function() {
     for(var i = 0; i < hours.length; i++) { // for each hours
@@ -34,57 +50,59 @@ Store.prototype.fakeCustomerVisits = function() {
         // hrCookies[i] = this.hrCustomer[i] * this.avgCookie; use .push
         this.hrCookies.push(Math.round(this.hrCustomer[i] * this.avgCookie));
         // add current hrCookies to totalCookies
-        this.totalCookies += this.hrCookies[i]; 
+        this.totalCookies += this.hrCookies[i];
     } // end for loop
-} // end fakeCustomerVisits  
+} // end fakeCustomerVisits
 
-Store.prototype.render = function(tableId) {
+Store.prototype.renderSales = function(tableId) {
     // ?? assume this is called after table id has been grabbed ??
-    // that won't work because of variable scope. 
-    // perhaps could have render return a value that is the table row
-    var tableEl = document.getElementById(tableId);
-    // make a tr
-    var trEl = document.createElement('tr');
-    var tdEl = document.createElement('td'); 
-    tdEl.textContent = this.storeName; 
+    // that won't work because of variable scope.
+    // perhaps could have renderSales return a value that is the table row
+    var tableEl = document.getElementById(tableId); // get table by Id
+    var trEl = document.createElement('tr'); // a tr that will hold the td elements
+    var tdEl = document.createElement('td'); // first td is storeName
+    tdEl.textContent = this.storeName;
     trEl.appendChild(tdEl);
-    for(var i in hours) {
-        var tdEl = document.createElement('td'); 
-        tdEl.textContent = '' + this.hrCookies[i]; 
+    for(var i = 0; i < hours.length; i++) {
+        var tdEl = document.createElement('td');
+        tdEl.textContent = '' + this.hrCookies[i];
         trEl.appendChild(tdEl);
     } // end for loop for all sales hours
-    tdEl.textContent = '' + this.totalCookies; 
+    tdEl = document.createElement('td');
+    tdEl.textContent = '' + this.totalCookies;
     trEl.appendChild(tdEl);
-    tableEl.appendChild(trEl); 
-    
-    // for each datacolumn
-        // make td
-        // input text
-        // put td in tr
-    // put tr on document
+    tableEl.appendChild(trEl);
+} // end renderSales method
 
+Store.prototype.renderStaff = function(tableId, storeMinStaff, storeStaffWorkload) {
+    // outputs to table with tableId
+    // storeMinStaff is minimum number of staff, regardless of store volume, at a location
+    // storeStaffWorkload is how many cookies/hr for a worker, if we sell more per hour, add another staff member
+    // currently storeMinStaff & storeStaffWorkload are always going to call minStaff & staffWorkload as set for all stores
+    // potentially could have different values for different store locations, by calling this method with different values
 
-    // make the table info here
-} // end render method
-
-            // oldRender: function() {
-            //     // get ul by id
-            //     var list = document.getElementById('sales_store' + this.storeNum); 
-            //     for (var i = 0; i < hours.length; i++) { // in a loop for each work hour
-            //         var newLi = document.createElement('li');
-            //         newLi.textContent = '' + hours[i] + ': ' + this.hrCookies[i] + ' cookies';
-            //         list.appendChild(newLi);
-            //     } // end loop of make li, put data in li, place li in DOM
-            //     var lastLi = document.createElement('li'); 
-            //     lastLi.textContent = 'Total: ' + this.totalCookies + ' cookies';
-            //     lastLi.setAttribute('class', 'sales_total');
-            //     list.appendChild(lastLi);
-            // } // end render: function()
+    var tableEl = document.getElementById(tableId); // get table by Id
+    var trEl = document.createElement('tr'); // make a tr that holds some td
+    var tdEl = document.createElement('td'); // first td is storeName
+    tdEl.textContent = this.storeName;
+    trEl.appendChild(tdEl);
+    for(var i in hours) {
+        var staffNeeded = Math.max(storeMinStaff, Math.ceil(this.hrCookies[i] / storeStaffWorkload));
+        // staffNeeded will be at least minStaff, or 1 worker per staffWorkload of cookies
+        // we will always assume to add staff if we go over by 1 or more
+        var tdEl = document.createElement('td');
+        tdEl.textContent = '' + staffNeeded; 
+        trEl.appendChild(tdEl);
+    } // end for loop for all sales hours
+    trEl.appendChild(tdEl);
+    tableEl.appendChild(trEl);
+} // end renderStaff method
 
 // ==================
 // create store objects with known variables
 // ==================
 
+// info given to use: 
 // First and Pike, 23, 65, 6.3
 // SeaTac Airport, 3, 24, 1.2
 // Seattle Center, 11, 38, 3.7
@@ -92,57 +110,55 @@ Store.prototype.render = function(tableId) {
 // Alki, 2, 16, 4.6
 
 new Store('First and Pike', 23, 65, 6.3);
-new Store('SeaTac Airport', 3, 24, 1.2); 
-new Store('Seattle Center', 11, 38, 3.7); 
-new Store('Capitol Hill', 20, 38, 2.3); 
-new Store('Alki', 2, 16, 4.6); 
+new Store('SeaTac Airport', 3, 24, 1.2);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('Capitol Hill', 20, 38, 2.3);
+new Store('Alki', 2, 16, 4.6);
 
 // ==================
-// functions potentially used in a few place 
+// functions potentially used in a few place
 // ==================
 
 function randomCount(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-// at some place, make sure to call fakeCustomerVisits methods for each object
-// but I believe we figured how to do this object construction? 
 
-// function fillFakeCustomerVisits() {
-//     // for(var i of storeList) {
-//     for(var i = 0; i <storeList.length; i++) { // for store object
-//         storeList[i].fakeCustomerVisits();
-//     } // end for loop
-// } // end fillFakeCustomerVisits
+function tableHeader(tableId, tableHeaderList) { // asumes header on top row
+    var tableEl = document.getElementById(tableId); // grab table id
+    var trEl = document.createElement('tr'); // make header row
+    for(var i in tableHeaderList) {
+        var thEl = document.createElement('th');
+        thEl.textContent = '' + tableHeaderList[i];
+        trEl.appendChild(thEl);
+    } // end for loop for all sales hours
+    tableEl.appendChild(trEl);
+} // end function tableHeader
 
-// fillFakeCustomerVisits();
+function tableFooter(tableId, tableFooterList) {
+    // this is where totals for each hour is output
+
+} // end function tableFooter
+
+function salesTable(tableId, tableHeaderList) {
+    tableHeader(tableId, tableHeaderList);
+    for(var i = 0; i < storeList.length; i++) {
+        storeList[i].renderSales(tableId);
+    }
+    var tableFoot = ''; 
+    tableFooter(tableId, tableFoot); 
+} // end function salesTable
+
+function staffTable(tableId, tableHeaderList) {
+    tableHeader(tableId, tableHeaderList);
+    for(var i in storeList) {
+        storeList[i].renderStaff(tableId, minStaff, staffWorkload);
+    }
+} // end function staffTable
 
 // ==================
 // using all objects in storeList, build the full table
 // make sure to have the header row of table
 // ==================
 
-function makeTable(tableId) {
-    // grab table id
-    var tableEl = document.getElementById(tableId);
-    // make header row
-    var trEl = document.createElement('tr');
-    var thEl = document.createElement('th'); 
-    thEl.textContent = 'Store Name'; 
-    trEl.appendChild(thEl);
-    for(var i in hours) {
-        var thEl = document.createElement('th'); 
-        thEl.textContent = '' + hours[i]; 
-        trEl.appendChild(thEl);
-    } // end for loop for all sales hours
-    thEl.textContent = 'Daily Location Total';
-    trEl.appendChild(thEl);
-    tableEl.appendChild(trEl); 
-        // for each storeList 
-        for(var i = 0; i < storeList.length; i++) {
-            storeList[i].render(tableId); 
-        }
-        // call the render method to make the row
-    // done? 
-} 
-
-makeTable('sales-data'); 
+salesTable('sales-data', salesHeader);
+staffTable('staff-data', staffHeader);
